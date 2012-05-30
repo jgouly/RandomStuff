334f96a61ed095187833b5a3e3f115b061e93f32
joey <cardologist@gmail.com>
2012-05-30 22:58:52 +0100

    Tidy things up, help from mhw

--- QuadChallenges/ch32.scm
+++ QuadChallenges/ch32.scm
@@ -1,29 +1,25 @@
-(define eps 0.0001)
+(define eps 0.001)
 
 (define (my-tan x)
   (if (< x eps)
-      (begin (display x) (newline) x)
-      
+      x
       (let ((t2 (my-tan (/ x 2))))
 	(/ (* 2 t2)
 	   (- 1 (* t2 t2))))))
 
 
-
-(define (do-tan x)
-  (/ (* 2 x)
-     (- 1 (* x x))))
-
 (define (find-smallest x)
-  (let lewp ((n x))
+  (let lewp ((n x) (i 0))
     (if (< n eps)
-	n
-	(lewp (/ n 2)))))
+	(cons n i)
+	(lewp (/ n 2) (+ i 1)))))
 
 
 (define (tan-iter x)
-  (let ((smallest (find-smallest x)))
-    (let lewp ((start smallest) (current smallest))
-      (if (>= current x)
+  (let ((prep (find-smallest x))
+	(my-tan (lambda (x) (/ (* 2 x)
+			       (- 1 (* x x))))))
+    (let lewp ((start (car prep)) (n (cdr prep)))
+      (if (= 0 n)
 	  start
-	  (lewp (do-tan start) (* 2 current))))))
+	  (lewp (my-tan start) (- n 1))))))
